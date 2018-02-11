2c1164bb5dfac0a10d447f92d0103b044f68980b
jgerson <jeremy.gerson@gmail.com>
2018-02-11 15:42:50 -0500

    3 vms now

--- main_aws.tf
+++ main_aws.tf
@@ -42,6 +42,24 @@ resource "aws_instance" "example2" {
   }
 }
 
+resource "aws_instance" "example3" {
+  # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type in us-east-1
+  count = 1
+  ami = "ami-97785bed"
+  instance_type = "t2.medium"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World test" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags {
+    Name = "terraform-example 3"
+  }
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance 2"
 
