0d16037f810a052bc734435fd94299b7ea62afe3
jgerson <jeremy.gerson@gmail.com>
2018-02-11 15:24:19 -0500

    correcting output

--- main_aws.tf
+++ main_aws.tf
@@ -24,24 +24,6 @@ resource "aws_instance" "example1" {
   }
 }
 
-resource "aws_instance" "example2" {
-  # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type in us-east-1
-  count = 1
-  ami = "ami-97785bed"
-  instance_type = "t2.medium"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World test" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
-
-  tags {
-    Name = "terraform-example 2"
-  }
-}
-
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance 2"
 
