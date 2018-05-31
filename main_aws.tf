f3efdaab9e7d8da7dbd71ba49c6effdc3ce7edbc
Jeremy Gerson <jgerson@hashicorp.com>
2018-05-31 17:35:08 -0400

    Update main_aws.tf

--- main_aws.tf
+++ main_aws.tf
@@ -24,7 +24,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type in us-east-1
   count = 1
-  ami = "ami-0663220831b3ae69b"
+  ami = "ami-2d39803a"
   instance_type = "t2.medium"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
